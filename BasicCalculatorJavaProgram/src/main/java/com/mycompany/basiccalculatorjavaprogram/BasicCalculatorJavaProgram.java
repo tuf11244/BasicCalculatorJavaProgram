bbafbe268feddcbd042a342c31691fe8551b7909
Parth Desai <12115060+tuf11244@users.noreply.github.com>
2025-09-01 19:20:16 -0400

    Committing changes

---
+++ BasicCalculatorJavaProgram/src/main/java/com/mycompany/basiccalculatorjavaprogram/BasicCalculatorJavaProgram.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.basiccalculatorjavaprogram;
+
+/**
+ *
+ * @author parth
+ */
+public class BasicCalculatorJavaProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My name is Parth");
+    }
+}
